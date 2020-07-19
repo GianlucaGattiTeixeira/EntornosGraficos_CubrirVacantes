e5607e40f04514174a712cd182e1b04f8b07d605
DanielDruetta <danidruetta_97@hotmail.com>
2020-07-19 19:40:23 -0300

    contacto

--- Logic/contacto.php
+++ Logic/contacto.php
@@ -6,11 +6,50 @@
         ?>
     </head>
     
- 	<body>
-      <br></br>
-      <div class="container">
-        <h3>Acá van los datos de contacto </h3>
-      </div>
+  <body>
+    <div class="container">
+			<form action="../Logic/contacto2.php" method="post">
+				<div class="form-group col-md-12">
+					<br/>
+					<h3>Contacténos</h3>
+        </div>
+        
+        <div class="form-group">
+					<label class="control-label col-md-2">Nombre:</label>
+					<div class="col-md-10">
+					  <input name="nombre" class="form-control" type="text" placeholder="" required>
+					</div>
+        </div>
+        
+        <div class="form-group">
+					<label class="control-label col-md-2">Email:</label>
+					<div class="col-md-10">
+					  <input name="email" class="form-control" type="email" placeholder="" required>
+					</div>
+        </div>
+        
+				<div class="form-group">
+					<label class="control-label col-md-2">Asunto:</label>
+					<div class="col-md-10">
+					  <input name="asunto" class="form-control" type="text" placeholder="" required>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<label class="control-label col-md-2">Consulta:</label>
+					<div class="col-md-10">
+            <textarea name="consulta" class="form-control" rows="5" required></textarea>
+					</div>
+				</div>
+
+				<div class="form-group">
+					<div class="col-md-2">
+						<input name="enviar" type="submit" class="btn btn-primary">           
+					</div>
+				</div>
+			
+			</form>
+		</div>
       
       <?php
         include_once("../Logic/footer.php");
