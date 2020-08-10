d3f648c7f5ef5021966566aba6c0815daf5a78a3
AgustinYurescia <agustin_yurescia@hotmail.com>
2020-08-10 15:16:13 -0300

    Cambio form de registro

--- Logic/registrarse.php
+++ Logic/registrarse.php
@@ -14,65 +14,72 @@
 				<br />
 				<h3>Registrarse</h3>
 			</div>
-			<div class="form-group">
-				<label for="inputDni" class="control-label col-md-2">DNI:</label>
-				<div class="col-md-10">
-					<input id="inputDni" name="dni" class="form-control" type="number" placeholder="">
+			<div class="form-row">
+				<div class="form-group col-md-4">
+					<label for="inputDni" class="control-label col-md-2">DNI:</label>
+					<div class="col-md-12">
+						<input id="inputDni" name="dni" class="form-control" type="number" placeholder="">
+					</div>
+				</div>
+
+				<div class="form-group col-md-4">
+					<label for="inputNombre" class="control-label col-md-2">Nombre:</label>
+					<div class="col-md-12">
+						<input id="inputNombre" name="nombre" class="form-control" type="text" placeholder="">
+					</div>
+				</div>
+
+				<div class="form-group col-md-4">
+					<label for="inputApellido" class="control-label col-md-2">Apellido:</label>
+					<div class="col-md-12">
+						<input id="inputApellido" name="apellido" class="form-control" type="text" placeholder="">
+					</div>
 				</div>
 			</div>
 
-			<div class="form-group">
-				<label for="inputNombre" class="control-label col-md-2">Nombre:</label>
-				<div class="col-md-10">
-					<input id="inputNombre" name="nombre" class="form-control" type="text" placeholder="">
+			<div class="form-row">
+				<div class="form-group col-md-4">
+					<label for="inputUsuario" class="control-label col-md-2">Usuario:</label>
+					<div class="col-md-12">
+						<input id="inputUsuario" name="usuario" class="form-control" type="text" placeholder="">
+					</div>
+				</div>
+
+				<div class="form-group col-md-4">
+					<label for="inputContrasena" class="control-label col-md-2">Contrasena:</label>
+					<div class="col-md-12">
+						<input id="inputContrasena" name="contrasena" class="form-control" type="password">
+					</div>
+				</div>
+
+				<div class="form-group col-md-4">
+					<label for="inputEmail" class="control-label col-md-2">Email:</label>
+					<div class="col-md-12">
+						<input id="inputEmail" name="email" class="form-control" type="email" placeholder="">
+					</div>
 				</div>
 			</div>
 
-			<div class="form-group">
-				<label for="inputApellido" class="control-label col-md-2">Apellido:</label>
-				<div class="col-md-10">
-					<input id="inputApellido" name="apellido" class="form-control" type="text" placeholder="">
+			<div class="form-row">
+				<div class="form-group col-md-8">
+					<label for="inputDireccion" class="control-label col-md-2">Direccion:</label>
+					<div class="col-md-12">
+						<input id="inputDireccion" name="direccion" class="form-control" type="text" placeholder="Calle-Nro-Piso-Depto">
+					</div>
 				</div>
-			</div>
-			<div class="form-group">
-				<label for="inputUsuario" class="control-label col-md-2">Usuario:</label>
-				<div class="col-md-10">
-					<input id="inputUsuario" name="usuario" class="form-control" type="text" placeholder="">
-				</div>
-			</div>
 
-			<div class="form-group">
-				<label for="inputContrasena" class="control-label col-md-2">Contrasena:</label>
-				<div class="col-md-10">
-					<input id="inputContrasena" name="contrasena" class="form-control" type="password">
-				</div>
-			</div>
-
-			<div class="form-group">
-				<label for="inputEmail" class="control-label col-md-2">Email:</label>
-				<div class="col-md-10">
-					<input id="inputEmail" name="email" class="form-control" type="email" placeholder="">
-				</div>
-			</div>
-
-			<div class="form-group">
-				<label for="inputDireccion" class="control-label col-md-2">Direccion:</label>
-				<div class="col-md-10">
-					<input id="inputDireccion" name="direccion" class="form-control" type="text" placeholder="">
-				</div>
-			</div>
-
-			<div class="form-group">
-				<label for="inputCiudad" class="control-label col-md-2">Ciudad:</label>
-				<div class="col-md-10">
-					<input id="inputCiudad" name="ciudad" class="form-control" type="text" placeholder="">
+				<div class="form-group col-md-4">
+					<label for="inputCiudad" class="control-label col-md-2">Ciudad:</label>
+					<div class="col-md-12">
+						<input id="inputCiudad" name="ciudad" class="form-control" type="text" placeholder="">
+					</div>
 				</div>
 			</div>
 
 
 			<div class="form-group">
 				<div class="col-md-2">
-					<button type="submit" class="btn btn-primary">Ingresar</button>
+					<button type="submit" class="btn btn-primary">Registrarse</button>
 				</div>
 			</div>
 
